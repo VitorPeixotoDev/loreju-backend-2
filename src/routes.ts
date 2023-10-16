5144f7cdb857621c692600a4c57c69a23e6dcbb4
vitor2204 <vitorpeixotoc137@gmail.com>
2023-10-16 18:42:40 -0300

    pre-db

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,11 @@ import { Router, Request, Response } from 'express'
 const router = Router()
 
 router.get('/test', (req: Request, res: Response) => {
-    return res.json({ok: true})
+    return res.json({name: "loreju-backend"})
+})
+
+router.get('/error', (req: Request, res: Response) => {
+    throw new Error('[ALARM 01202]')
 })
 
 export { router }
