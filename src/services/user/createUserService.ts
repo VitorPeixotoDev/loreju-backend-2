552d0340181ed0024f14bb4b2a9b7e9cfc30411c
vitor2204 <vitorpeixotoc137@gmail.com>
2023-10-17 18:07:51 -0300

    register user

--- src/services/user/createUserService.ts
+++ src/services/user/createUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from "../../prisma"
+
 interface UserRequest {
     name: string
     email: string
@@ -6,8 +8,39 @@ interface UserRequest {
 
 class CreateUserService {
     async execute({name, email, password}: UserRequest){
-        console.log(name)
-        return { name }
+
+        //verificar informações
+        if(!email){
+            throw new Error("Email incorretct")
+        }
+
+        //verificar se email já está cadastrado
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+
+        if(userAlreadyExists){
+            throw new Error("User already exists")
+        }
+
+        //cadastrar usuário
+        const user = await prismaClient.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: password
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true
+            }
+        })
+
+        console.log(user)
+        return { user }
     }
 }
 
