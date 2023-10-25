e4def09f0c0e7db61f126471b5f0e410481b1c1f
vitor2204 <vitorpeixotoc137@gmail.com>
2023-10-25 18:18:43 -0300

    show all details at the order

--- src/routes.ts
+++ src/routes.ts
@@ -13,6 +13,7 @@ import { AddItemController } from './controllers/order/AddItemController'
 import { RemoveItemController } from './controllers/order/RemoveItemController'
 import { SendOrderController } from './controllers/order/SendOrderController'
 import { ListOrdersController } from './controllers/order/ListOrdersController'
+import { DetailOrderController } from './controllers/order/DetailOrderController'
 import uploadConfig from './config/multer'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
@@ -41,6 +42,7 @@ router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
 router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 router.get('/orders', isAuthenticated, new ListOrdersController().handle)
+router.get('/order/detail', isAuthenticated, new DetailOrderController().handle)
 
 
 // ROUTES TEST
